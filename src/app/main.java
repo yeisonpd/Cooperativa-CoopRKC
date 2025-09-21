0a675cfa7a7f0d72d4d270539c99e2478578850c
SpaiditDev <yeisonpadronhiguita@gmail.com>
2025-09-21 15:11:02 -0500

    udate + clase socio

--- src/app/main.java
+++ src/app/main.java
@@ -1,5 +1,7 @@
 package app;
 
 public class main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- src/modelo/socio.java
+++ src/modelo/socio.java
@@ -1,5 +1,34 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class socio {
-    
+    //variables
+    private String nombre;
+    private String cedula;
+    private List<cuenta> cuentas;
+
+    //constructor
+    public socio(String nombre, String cedula){
+        this.nombre = nombre;
+        this.cedula = cedula;
+        this.cuentas = new ArrayList<>();
+    }
+
+    //getters
+    public String getNombre(){
+        return nombre;
+    }
+
+    public String getCedula(){
+        return cedula;
+    }
+
+    public List<cuenta> getCuentas(){
+        return cuentas;
+    }
+
+    //setters
+
 }
